3e52467fedb12c045ccdc687f4638d128fc28303
Zulfan Faizun Najib <121880142+faeznz@users.noreply.github.com>
2024-12-03 00:23:09 +0700

    feat: finish slicing splash screen

--- lib/screen/splash/splash_screen.dart
+++ lib/screen/splash/splash_screen.dart
@@ -1,22 +1,87 @@
-import 'package:easy_splash_screen/easy_splash_screen.dart';
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:predict_anemia/constant/color_constant.dart';
+import 'package:predict_anemia/constant/text_style_constant.dart';
 import 'package:predict_anemia/screen/welcome/welcome_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
-  State<SplashScreen> createState() => _SplashScreenState();
+  _SplashScreenState createState() => _SplashScreenState();
 }
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const WelcomeScreen()),
+      );
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return EasySplashScreen(
-      logo: Image.network(
-          'https://cdn4.iconfinder.com/data/icons/logos-brands-5/24/flutter-512.png'),
-      durationInSeconds: 2,
-      navigator: const WelcomeScreen(),
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          color: ColorConstant.primaryColor,
+          image: const DecorationImage(
+            image: AssetImage('assets/images/doc_1.png'),
+            fit: BoxFit.contain,
+            alignment: Alignment(0, 1),
+          ),
+        ),
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 80,
+                  horizontal: 40,
+                ),
+                child: SizedBox(
+                  width: double.infinity,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      RichText(
+                        text: TextSpan(
+                          text: 'Anemia\n',
+                          style: TextStyleConstant.montserratBlack.copyWith(
+                            color: ColorConstant.whiteColor,
+                            fontSize: 56,
+                            height: 1.0,
+                          ),
+                          children: [
+                            TextSpan(
+                              text: 'Predict!',
+                              style: TextStyleConstant.montserratBlack.copyWith(
+                                color: ColorConstant.whiteColor,
+                                fontSize: 40,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Text(
+                        'Ketahui anemia lewat telapak tangan kamu!',
+                        style: TextStyleConstant.montserratNormal.copyWith(
+                          fontSize: 14,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
